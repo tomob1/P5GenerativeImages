9d3acceb0ddcf5ac3c866cd21ae541feb89d3db9
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-12-09 13:06:35 +0000

    Add files via upload
    Added image

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,24 @@
-function setup() {
-    var canvas = createCanvas(800, 800);
-    canvas.parent("p5container");
+let img;
+let cnv;
+function preload() {
+    img = loadImage('Sunset.jpg')
 }
+
+function setup() {
+    cnv = createCanvas(img.width,img.height);
+    // let newCanvasX = (windowWidth - img.width)/2
+    // let newCanvasY = (windowHeight - img.height)/2
+    // cnv.position(newCanvasX, newCanvasY)
+  
+    // noLoop();
+
+
+}
+
 
 function draw() {
-    background(220);
-
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    background (220);
+    image (img, 0, 0);
+   
 }
+
